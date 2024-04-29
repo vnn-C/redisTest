63fd5988e6dee7edd3bae3fe8b710f777fce2db1
Vnn-C <vannienguyen993@gmail.com>
2024-04-28 21:56:45 -0500

    Notion test

--- src/app/data.tsx
+++ src/app/data.tsx
@@ -3,29 +3,63 @@
 //import { NotionPage } from "@/components/notion";
 //import renderTree() function for retrieving block children
 
+//database structure: (Subject to change)
+//page name's page id - pageName : pageId
+//page contents - pageNameContents : array of block children obtained through renderTree()
+//page slug - pageNameSlug : url slug for page
+//page properties - pageNameProps : json of page properties from retrieving notion page
 
+//inserts readable page name's pageId into database
 
-//inserts readable page name into database
-export async function setPageName(pageId: any, pageName: any){
-    fetch(`${process.env.KV_REST_API_URL}/set/${pageId}Name`, {
+const renderTree = (blockId: string) => {
+  fetch(`https://api.notion.com/v1/blocks/${blockId}/children`, {
+      headers: {
+          'Authorization': 'Bearer secret_vdJTeZt1yTHBG4Uio4eJTfKuqAfOEj8SjbyzlwPmPRn',
+          'Notion-Version': '2022-06-28'
+      }
+  })
+  .then(response => {
+      if(!response.ok) {
+          throw new Error('Network response not ok');
+      }
+      return response.json();
+  })
+  .then(data => {
+      data.results.forEach((block: any) => {
+          //console.log(block);
+          if(block.has_children) {
+              if(block.child_page){
+                  console.log(block.child_page.title);
+              }
+              renderTree(block.id);
+          }
+      });
+  })
+  .catch(error => {
+      console.error("Issue with fetch operation", error);
+  });
+}
+
+export async function setPageName(pageName: any, pageId: any){
+    fetch(`${process.env.KV_REST_API_URL}/set/${pageName}Name`, {
         headers: {
             Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
           },
-          body: JSON.stringify(pageName),
+          body: JSON.stringify(pageId),
           method: 'POST',
     })
     .then((response) => {
         if(!response.ok){
-            throw new Error(`Error with setting ${pageId} name`);
+            throw new Error(`Error with setting ${pageName} name`);
         }
-        console.log(`${pageId} properties set`);
+        console.log(`${pageName} properties set`);
        return response.json()});
 
 }
 
-//gets readable page name from database
-export async function getPageName(pageId: any){
-	fetch(`${process.env.KV_REST_API_URL}/get/${pageId}Name`, {
+//gets readable page name's pageId from database
+export async function getPageName(pageName: any){
+	fetch(`${process.env.KV_REST_API_URL}/get/${pageName}Name`, {
   	headers: {
     		Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
   		},
@@ -35,25 +69,15 @@ export async function getPageName(pageId: any){
         
 }
 
-//gets page properties from redis database
-export async function getPageProps(pageId: any){
-	fetch(`${process.env.KV_REST_API_URL}/get/${pageId}Props`, {
-  	headers: {
-    		Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
-  		},
-	})
-  		.then((response) => response.json())
-        .then((data) => console.log(data));
-}
 
 //inserts page properties into redis database
-export async function setPageProps(pageId: any, props: any){
+export async function setPageProps(pageName: any, props: any){
 
     //get page properties of given page separately
     //const props = []; //plaeholder
-    //const props = fetch(`https://api.notion.com/v1/blocks/${pageId}`); //fetch page properties
+    //const props = fetch(`https://api.notion.com/v1/blocks/${pageName}`); //fetch page properties
 
-    fetch(`${process.env.KV_REST_API_URL}/set/${pageId}Props`, {
+    fetch(`${process.env.KV_REST_API_URL}/set/${pageName}Props`, {
         headers: {
             Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
           },
@@ -62,16 +86,49 @@ export async function setPageProps(pageId: any, props: any){
         })
           .then((response) => {
             if(!response.ok){
-                throw new Error(`Error with setting ${pageId} properties`);
+                throw new Error(`Error with setting ${pageName} properties`);
             }
-            console.log(`${pageId} properties set`);
+            console.log(`${pageName} properties set`);
            return response.json()});
     
 }
 
+
+//gets page properties from redis database
+export async function getPageProps(pageName: any){
+	fetch(`${process.env.KV_REST_API_URL}/get/${pageName}Props`, {
+  	headers: {
+    		Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
+  		},
+	})
+  		.then((response) => response.json())
+        .then((data) => console.log(data));
+}
+
+//inserts page content into redis database
+export async function setPageContent(pageName: any, pageContent: any){
+
+  //get page content from pageName first
+  //const pageContent = []; //placeholder
+  //pageContent = renderTree(pageName); 
+  fetch(`${process.env.KV_REST_API_URL}/set/${pageName}Content`, {
+      headers: {
+          Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
+        },
+        body: JSON.stringify(renderTree("52505493f6fb488abe790d1a379d8275")),
+        method: 'POST',
+      })
+        .then((response) => {
+          if(!response.ok){
+              throw new Error(`Error with setting ${pageName} content`);
+          }
+          console.log(`${pageName} content set`);
+         return response.json()});
+}
+
 //gets page content from redis database
-export async function getPageContent(pageId: any){
-    fetch(`${process.env.KV_REST_API_URL}/get/${pageId}Content`, {
+export async function getPageContent(pageName: any){
+    fetch(`${process.env.KV_REST_API_URL}/get/${pageName}Content`, {
         headers: {
               Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
             },
@@ -80,30 +137,11 @@ export async function getPageContent(pageId: any){
               .then((data) => {return data});
 }
 
-//inserts page content into redis database
-export async function setPageContent(pageId: any, pageContent: any){
 
-    //get page content from pageId first
-    //const pageContent = []; //placeholder
-    //pageContent = renderTree(pageId); 
-    fetch(`${process.env.KV_REST_API_URL}/set/${pageId}Content`, {
-        headers: {
-            Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
-          },
-          body: JSON.stringify(pageContent),
-          method: 'POST',
-        })
-          .then((response) => {
-            if(!response.ok){
-                throw new Error(`Error with setting ${pageId} content`);
-            }
-            console.log(`${pageId} content set`);
-           return response.json()});
-}
 
 //inserts page slug into database
-export async function setPageSlug(pageId: any, pageSlug: any){
-    fetch(`${process.env.KV_REST_API_URL}/set/${pageId}Slug`, {
+export async function setPageSlug(pageName: any, pageSlug: any){
+    fetch(`${process.env.KV_REST_API_URL}/set/${pageName}Slug`, {
         headers: {
             Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
           },
@@ -112,16 +150,16 @@ export async function setPageSlug(pageId: any, pageSlug: any){
     })
     .then((response) => {
         if(!response.ok){
-            throw new Error(`Error with setting ${pageId} name`);
+            throw new Error(`Error with setting ${pageName} Slug`);
         }
-        console.log(`${pageId} properties set`);
+        console.log(`${pageName} properties set`);
        return response.json()});
 
 }
 
 //gets readable page slug from database
-export async function getPageSlug(pageId: any){
-	fetch(`${process.env.KV_REST_API_URL}/get/${pageId}Slug`, {
+export async function getPageSlug(pageName: any){
+	fetch(`${process.env.KV_REST_API_URL}/get/${pageName}Slug`, {
   	headers: {
     		Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
   		},
