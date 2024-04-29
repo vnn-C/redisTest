05c32796477f8c3657be9f3c49675bb071c934b7
Vnn-C <vannienguyen993@gmail.com>
2024-04-28 22:00:21 -0500

    Notion Test 2

--- src/app/data.tsx
+++ src/app/data.tsx
@@ -11,33 +11,37 @@
 
 //inserts readable page name's pageId into database
 
-const renderTree = (blockId: string) => {
-  fetch(`https://api.notion.com/v1/blocks/${blockId}/children`, {
-      headers: {
-          'Authorization': 'Bearer secret_vdJTeZt1yTHBG4Uio4eJTfKuqAfOEj8SjbyzlwPmPRn',
-          'Notion-Version': '2022-06-28'
+async function render(blockId: string){
+    const renderTree = (blockId: string) => {
+        fetch(`https://api.notion.com/v1/blocks/${blockId}/children`, {
+            headers: {
+                'Authorization': 'Bearer secret_vdJTeZt1yTHBG4Uio4eJTfKuqAfOEj8SjbyzlwPmPRn',
+                'Notion-Version': '2022-06-28'
+            }
+        })
+        .then(response => {
+            if(!response.ok) {
+                throw new Error('Network response not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            data.results.forEach((block: any) => {
+                //console.log(block);
+                if(block.has_children) {
+                    if(block.child_page){
+                        console.log(block.child_page.title);
+                    }
+                    renderTree(block.id);
+                }
+            });
+        })
+        .catch(error => {
+            console.error("Issue with fetch operation", error);
+        });
       }
-  })
-  .then(response => {
-      if(!response.ok) {
-          throw new Error('Network response not ok');
-      }
-      return response.json();
-  })
-  .then(data => {
-      data.results.forEach((block: any) => {
-          //console.log(block);
-          if(block.has_children) {
-              if(block.child_page){
-                  console.log(block.child_page.title);
-              }
-              renderTree(block.id);
-          }
-      });
-  })
-  .catch(error => {
-      console.error("Issue with fetch operation", error);
-  });
+
+    return renderTree;
 }
 
 export async function setPageName(pageName: any, pageId: any){
@@ -115,7 +119,7 @@ export async function setPageContent(pageName: any, pageContent: any){
       headers: {
           Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
         },
-        body: JSON.stringify(renderTree("52505493f6fb488abe790d1a379d8275")),
+        body: JSON.stringify(render("52505493f6fb488abe790d1a379d8275")),
         method: 'POST',
       })
         .then((response) => {
