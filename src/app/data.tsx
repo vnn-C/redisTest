4df6fcbd37e7827db308479d76f3e944b533d8b1
Vnn-C <vannienguyen993@gmail.com>
2024-05-03 17:00:03 -0500

    Notion Data Test

--- src/app/data.tsx
+++ src/app/data.tsx
@@ -1,19 +1,20 @@
 //import { redis } from '@/vercel/kv'
 //import { notion } from "@/notion"
 //import { NotionPage } from "@/components/notion";
-//import renderTree() function for retrieving block children
 
-//database structure: (Subject to change)
+//database structure used for data.tsx per notion page: (Subject to change)
 //page name's page id - pageName : pageId
 //page contents - pageNameContents : array of block children obtained through renderTree()
 //page slug - pageNameSlug : url slug for page
-//page properties - pageNameProps : json of page properties from retrieving notion page
+//page properties - pageNameProps : json of page properties from retrieving the notion page
+
+//TODO: add another record for value:key per notion page
+//pageId : pageName
+//array of block children obtained from renderTree() : pageNameContents
+//url slug for page : pageNameSlug
+//json of page properties fro mretrieving the notion page : pageNameProps
 
 //inserts readable page name's pageId into database
-
-    
-
-
 export async function setPageName(pageName: any, pageId: any){
     fetch(`${process.env.KV_REST_API_URL}/set/${pageName}Name`, {
         headers: {
@@ -26,7 +27,7 @@ export async function setPageName(pageName: any, pageId: any){
         if(!response.ok){
             throw new Error(`Error with setting ${pageName} name`);
         }
-        console.log(`${pageName} properties set`);
+        console.log(`${pageName} name set`);
        return response.json()});
 
 }
@@ -80,11 +81,11 @@ export async function getPageProps(pageName: any){
 }
 
 //inserts page content into redis database
+//page content is the results of renderTree()
+//calling GET on the key-value pair inside the vercel redis CLI reutrns {} instead of the results of renderTree.
+//This problem does not appear to be  present with the other key-value pairs.
+//This could just be a problem with the testing environment used to test data.tsx.
 export async function setPageContent(pageName: any, pageContent: any){
-
-  //get page content from pageName first
-  //const pageContent = []; //placeholder
-  //pageContent = renderTree(pageName); 
   fetch(`${process.env.KV_REST_API_URL}/set/${pageName}Content`, {
       headers: {
           Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
@@ -133,7 +134,7 @@ export async function setPageSlug(pageName: any, pageSlug: any){
 
 //gets readable page slug from database
 export async function getPageSlug(pageName: any){
-	fetch(`${process.env.KV_REST_API_URL}/get/${pageName}Slug`, {
+	fetch(`${process.env.KV_REST_API_URL}/get/${pageName}`, {
   	headers: {
     		Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
   		},
@@ -143,6 +144,112 @@ export async function getPageSlug(pageName: any){
         
 }
     
+//functions for value:key pairs
 
+//page name
+export async function setPageNameKey(pageName: any, pageId: any){
+  fetch(`${process.env.KV_REST_API_URL}/set/${pageId}`, {
+    headers: {
+        Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
+      },
+      body: JSON.stringify(pageName) + "Name",
+      method: 'POST',
+})
+.then((response) => {
+    if(!response.ok){
+        throw new Error(`Error with setting ${pageId} name key`);
+    }
+    console.log(`${pageId} name set`);
+   return response.json()});
+}
 
+export async function getPageNameKey(pageId: any){
+  fetch(`${process.env.KV_REST_API_URL}/get/${pageId}`, {
+  	headers: {
+    		Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
+  		},
+	})
+  		.then((response) => response.json())
+            .then((data) => console.log(data));
+}
 
+//page props
+export async function setPagePropsKey(pageName: any, pageProps: any){
+    fetch(`${process.env.KV_REST_API_URL}/set/${pageProps}`, {
+      headers: {
+          Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
+        },
+        body: JSON.stringify(pageName) + "Props",
+        method: 'POST',
+    })
+    .then((response) => {
+      if(!response.ok){
+          throw new Error(`Error with setting ${pageName} name`);
+      }
+      console.log(`${pageName} name set`);
+    return response.json()});
+}
+
+export async function getPagePropsKey(pageProps: any){
+  fetch(`${process.env.KV_REST_API_URL}/get/${pageProps}`, {
+  	headers: {
+    		Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
+  		},
+	})
+  		.then((response) => response.json())
+            .then((data) => console.log(data));
+}
+
+//page slugs
+export async function setPageSlugKey(pageName: any, pageSlug: any){
+  fetch(`${process.env.KV_REST_API_URL}/set/${pageSlug}`, {
+    headers: {
+        Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
+      },
+      body: JSON.stringify(pageName) + "Slug",
+      method: 'POST',
+})
+.then((response) => {
+    if(!response.ok){
+        throw new Error(`Error with setting ${pageName} name`);
+    }
+    console.log(`${pageName} name set`);
+   return response.json()});
+}
+
+export async function getPageSlugKey(pageSlug: any){
+  fetch(`${process.env.KV_REST_API_URL}/get/${pageSlug}`, {
+  	headers: {
+    		Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
+  		},
+	})
+  		.then((response) => response.json())
+            .then((data) => console.log(data));
+}
+
+//page content
+export async function setPageContentKey(pageName: any, pageContent: any){
+  fetch(`${process.env.KV_REST_API_URL}/set/${pageContent}`, {
+    headers: {
+        Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
+      },
+      body: JSON.stringify(pageName) + "Content",
+      method: 'POST',
+  })
+  .then((response) => {
+      if(!response.ok){
+          throw new Error(`Error with setting ${pageName} name`);
+      }
+      console.log(`${pageName} name set`);
+    return response.json()});
+}
+
+export async function getPageContentKey(pageContent: any){
+  fetch(`${process.env.KV_REST_API_URL}/get/${pageContent}`, {
+  	headers: {
+    		Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
+  		},
+	})
+  		.then((response) => response.json())
+            .then((data) => console.log(data));
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,181 @@ import { stringify } from "querystring";
 export default function Home() {
   var arr: any[] = [];
 
-  const renderTree = (blockId: string) => {
-    
+  const testProps = {
+    "object": "page",
+    "id": "59833787-2cf9-4fdf-8782-e53db20768a5",
+    "created_time": "2022-03-01T19:05:00.000Z",
+    "last_edited_time": "2022-07-06T19:16:00.000Z",
+    "created_by": {
+      "object": "user",
+      "id": "ee5f0f84-409a-440f-983a-a5315961c6e4"
+    },
+    "last_edited_by": {
+      "object": "user",
+      "id": "ee5f0f84-409a-440f-983a-a5315961c6e4"
+    },
+    "cover": {
+      "type": "external",
+      "external": {
+        "url": "https://upload.wikimedia.org/wikipedia/commons/6/62/Tuscankale.jpg"
+      }
+    },
+    "icon": {
+      "type": "emoji",
+      "emoji": "🥬"
+    },
+    "parent": {
+      "type": "database_id",
+      "database_id": "d9824bdc-8445-4327-be8b-5b47500af6ce"
+    },
+    "archived": false,
+    "properties": {
+      "Store availability": {
+        "id": "%3AUPp"
+      },
+      "Food group": {
+        "id": "A%40Hk"
+      },
+      "Price": {
+        "id": "BJXS"
+      },
+      "Responsible Person": {
+        "id": "Iowm"
+      },
+      "Last ordered": {
+        "id": "Jsfb"
+      },
+      "Cost of next trip": {
+        "id": "WOd%3B"
+      },
+      "Recipes": {
+        "id": "YfIu"
+      },
+      "Description": {
+        "id": "_Tc_"
+      },
+      "In stock": {
+        "id": "%60%5Bq%3F"
+      },
+      "Number of meals": {
+        "id": "zag~"
+      },
+      "Photo": {
+        "id": "%7DF_L"
+      },
+      "Name": {
+        "id": "title"
+      }
+    },
+    "url": "https://www.notion.so/Tuscan-Kale-598337872cf94fdf8782e53db20768a5"
+  };
 
+
+  const testArr = {
+    "object": "list",
+    "results": [
+      {
+        "object": "block",
+        "id": "c02fc1d3-db8b-45c5-a222-27595b15aea7",
+        "parent": {
+          "type": "page_id",
+          "page_id": "59833787-2cf9-4fdf-8782-e53db20768a5"
+        },
+        "created_time": "2022-03-01T19:05:00.000Z",
+        "last_edited_time": "2022-03-01T19:05:00.000Z",
+        "created_by": {
+          "object": "user",
+          "id": "ee5f0f84-409a-440f-983a-a5315961c6e4"
+        },
+        "last_edited_by": {
+          "object": "user",
+          "id": "ee5f0f84-409a-440f-983a-a5315961c6e4"
+        },
+        "has_children": false,
+        "archived": false,
+        "type": "heading_2",
+        "heading_2": {
+          "rich_text": [
+            {
+              "type": "text",
+              "text": {
+                "content": "Lacinato kale",
+                "link": null
+              },
+              "annotations": {
+                "bold": false,
+                "italic": false,
+                "strikethrough": false,
+                "underline": false,
+                "code": false,
+                "color": "default"
+              },
+              "plain_text": "Lacinato kale",
+              "href": null
+            }
+          ],
+          "color": "default",
+          "is_toggleable": false
+        }
+      },
+      {
+        "object": "block",
+        "id": "acc7eb06-05cd-4603-a384-5e1e4f1f4e72",
+        "parent": {
+          "type": "page_id",
+          "page_id": "59833787-2cf9-4fdf-8782-e53db20768a5"
+        },
+        "created_time": "2022-03-01T19:05:00.000Z",
+        "last_edited_time": "2022-03-01T19:05:00.000Z",
+        "created_by": {
+          "object": "user",
+          "id": "ee5f0f84-409a-440f-983a-a5315961c6e4"
+        },
+        "last_edited_by": {
+          "object": "user",
+          "id": "ee5f0f84-409a-440f-983a-a5315961c6e4"
+        },
+        "has_children": false,
+        "archived": false,
+        "type": "paragraph",
+        "paragraph": {
+          "rich_text": [
+            {
+              "type": "text",
+              "text": {
+                "content": "Lacinato kale is a variety of kale with a long tradition in Italian cuisine, especially that of Tuscany. It is also known as Tuscan kale, Italian kale, dinosaur kale, kale, flat back kale, palm tree kale, or black Tuscan palm.",
+                "link": {
+                  "url": "https://en.wikipedia.org/wiki/Lacinato_kale"
+                }
+              },
+              "annotations": {
+                "bold": false,
+                "italic": false,
+                "strikethrough": false,
+                "underline": false,
+                "code": false,
+                "color": "default"
+              },
+              "plain_text": "Lacinato kale is a variety of kale with a long tradition in Italian cuisine, especially that of Tuscany. It is also known as Tuscan kale, Italian kale, dinosaur kale, kale, flat back kale, palm tree kale, or black Tuscan palm.",
+              "href": "https://en.wikipedia.org/wiki/Lacinato_kale"
+            }
+          ],
+          "color": "default"
+        }
+      }
+    ],
+    "next_cursor": null,
+    "has_more": false,
+    "type": "block",
+    "block": {}
+  };
+
+
+
+
+
+
+  const renderTree = (blockId: string) => {
     fetch(`https://api.notion.com/v1/blocks/${blockId}/children`, {
         headers: {
             'Authorization': 'Bearer secret_vdJTeZt1yTHBG4Uio4eJTfKuqAfOEj8SjbyzlwPmPRn',
@@ -18,24 +190,20 @@ export default function Home() {
         if(!response.ok) {
             throw new Error('Network response not ok');
         }
-        const arr = [];
         return response.json();
     })
     .then(data => {
         data.results.forEach((block: any) => {
           
             console.log(block);
-            
             if(block.has_children) {
                 if(block.child_page){
                     
-                  arr.push(block.child_page);
+                  
                     console.log(block.child_page.title);
                 }
                 renderTree(block.id);
             }
-
-            return arr;
         });
     })
     .catch(error => {
@@ -44,16 +212,20 @@ export default function Home() {
   }
 
   console.log("Starting");
-  const test = data.setPageProps("SetTest", "Props");
+  const test = data.setPageProps("SetTest", JSON.stringify(testProps));
   const testFour = data.getPageProps("SetTestProps");
   console.log("Success");
   const testTwo = data.setPageName("SetTest", "Name");
-  arr.push(JSON.stringify(renderTree("c102c1df8f984db0b1aeb0dada4fb70a")));
+  renderTree("c102c1df8f984db0b1aeb0dada4fb70a");
   arr.push("Test Here");
-  const testThree = data.setPageContent("SetTest", arr);
+  const testThree = data.setPageContent("SetTest", JSON.stringify(testArr));
 
+  const testFive = data.setPagePropsKey("SetTest", JSON.stringify(testProps));
+  const testSix = data.setPageNameKey("SetTest", "Name");
+  const testSeven = data.setPageSlugKey("SetTest", "Slug");
+  const testEight = data.setPageContentKey("SetTest", JSON.stringify(testArr));
   
-
+  
   return (
     <main className={styles.main}>
       <div className={styles.description}>
